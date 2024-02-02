41c46fa77e3ec93ec85d6866e8826f65e7a0d878
OGOZ111 <djsmallboy@gmail.com>
2024-02-02 12:49:50 +0200

    finished store, and useSelector, to import data into Countries.Jsx to render out

--- src/routes/Countries.jsx
+++ src/routes/Countries.jsx
@@ -1,8 +1,84 @@
+import { useEffect } from "react";
+
+import { Spinner } from "react-bootstrap";
+import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import ListGroup from "react-bootstrap/ListGroup";
+import Row from "react-bootstrap/Row";
+import { useDispatch } from "react-redux";
+import { initializeCountries } from "../store/countriesSlice";
+import { useSelector } from "react-redux";
+
 const Countries = () => {
+  const dispatch = useDispatch();
+
+  // import the countriesList and loading state from the store
+
+  const countriesList = useSelector((state) => state.countries.countries);
+  const loading = useSelector((state) => state.countries.loading);
+
+  useEffect(() => {
+    dispatch(initializeCountries());
+  }, [dispatch]);
+
+  if (loading) {
+    return (
+      <Col className="text-center m-5">
+        <Spinner
+          animation="border"
+          role="status"
+          className="center"
+          variant="info"
+        >
+          <span className="visually-hidden">Loading...</span>
+        </Spinner>
+      </Col>
+    );
+  }
+
   return (
-    <div>
-      <h1>Countries</h1>
-    </div>
+    <Container fluid>
+      <Row xs={2} md={3} lg={4} className=" g-3">
+        {countriesList.map((country) => (
+          <Col key={country.name.official} className="mt-5">
+            <Card className="h-100">
+              <Card.Img
+                variant="top"
+                className="rounded h-50"
+                style={{
+                  objectFit: "cover",
+                  minHeight: "200px",
+                  maxHeight: "200px",
+                }}
+              />
+              <Card.Body className="d-flex flex-column">
+                <Card.Title>** PUT COUNTRY COMMON NAME HERE **</Card.Title>
+                <Card.Subtitle className="mb-5 text-muted">
+                  ** PUT COUNTRY OFFICIAL NAME HERE **
+                </Card.Subtitle>
+                <ListGroup
+                  variant="flush"
+                  className="flex-grow-1 justify-content-end"
+                >
+                  <ListGroup.Item>
+                    <i className="bi bi-translate me-2"></i>
+                    create a list of languages from the array.
+                  </ListGroup.Item>
+                  <ListGroup.Item>
+                    <i className="bi bi-cash-coin me-2"></i>
+                    create list of currencies from the array.
+                  </ListGroup.Item>
+                  <ListGroup.Item>
+                    show population with formatting here.
+                  </ListGroup.Item>
+                </ListGroup>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </Container>
   );
 };
 
--- src/store/countriesSlice.js
+++ src/store/countriesSlice.js
@@ -1,12 +1,33 @@
 import { createSlice } from "@reduxjs/toolkit";
+import countriesService from "../services/countries";
+
+export const initializeCountries = () => {
+  return async (dispatch) => {
+    const countries = await countriesService.getAll(); // axios call to get data
+    dispatch(getCountries(countries)); //put payload into reducer.
+    setTimeout(() => {
+      dispatch(isLoading(false));
+    }, 1000);
+  };
+};
 
 export const countriesSlice = createSlice({
   name: "countries",
   initialState: {
-    countries: [],
-    isLoading: true,
+    countries: [], // to access this state, use useSelector((state) => state.countries.countries)
+    isLoading: true, // to access this state, use useSelector((state) => state.countries.isLoading)
   },
-  reducers: {},
+  reducers: {
+    getCountries: (state, action) => {
+      state.countries = action.payload;
+      state.isLoading = false;
+    },
+    isLoading: (state, action) => {
+      state.isLoading = action.payload;
+    },
+  },
 });
+
+export const { getCountries, isLoading } = countriesSlice.actions;
 
 export default countriesSlice.reducer;
